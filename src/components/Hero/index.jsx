0d20aa4d8ff76ae17709b9dca2b26e5f029e0c10
Muhammad Omar <omar@thecodingwhiz.com>
2023-06-15 15:37:50 +0100

    Committing local changes before merging

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -3,10 +3,25 @@ import { Link } from "react-router-dom";
 import { Container, Card, Button } from "react-bootstrap";
 import "../../styles.css";
 
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <Link to="/" className="navbar-logo">
+        Logo
+      </Link>
+      <div className="navbar-links">
+        <Link to="/login">Log In</Link>
+        <Link to="/register">Register</Link>
+      </div>
+    </nav>
+  );
+};
+
 const Hero = () => {
   return (
-    <div className="py-5">
-      <Container className="d-flex justify-content-center">
+    <div className="hero-container">
+      <Navbar />
+      <Container className="d-flex justify-content-center align-items-center flex-column">
         <Card className="p-5 d-flex flex-column align-items-center hero-card bg-light w-75">
           <h1 className="text-center mb-4">
             Welcome to LinguaPlay: Where Language Learning and Gaming Unite!
@@ -15,14 +30,9 @@ const Hero = () => {
             Ready to level up your language skills? Join LinguaPlay and embark
             on an immersive learning adventure through gaming.
           </p>
-          <div className="d-flex">
-            <Button variant="primary" as={Link} to="/login" className="me-3">
-              Log In
-            </Button>
-            <Button variant="secondary" as={Link} to="/register">
-              Register
-            </Button>
-          </div>
+          <Button variant="primary" className="get-started-button">
+            Get Started
+          </Button>
         </Card>
       </Container>
     </div>
