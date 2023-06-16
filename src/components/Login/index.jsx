2328ce65822c762c1c66194cb13fad7d6737aeaf
Muhammad Omar <omar@thecodingwhiz.com>
2023-06-16 16:38:54 +0100

    Completed merge with origin/staging

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -34,41 +34,44 @@ const Login = () => {
   }
 
   return (
-    <div className="input-component">
-      <h1>Login Page</h1>
+    <div className="login-container">
+      <div className="login-box">
+        <h1 className="login-heading">Login Page</h1>
 
-      <form className="inputs">
-        <input
-          type="text"
-          aria-label="username input"
-          placeholder="Enter Username"
-          onChange={handleUsername}
-          value={username}
-          className="inputField"
-        ></input>
-        <input
-          type="password"
-          aria-label="password input"
-          placeholder="Enter Password"
-          onChange={handlePassword}
-          value={password}
-          className="inputField"
-        ></input>
-        <br />
-        <button
-          onClick={gatherDetails}
-          id="submitBtn"
-          aria-label="submit button"
-        >
-          Submit
-        </button>
-        <p>
-          Not registered?{" "}
-          <a href="/register" style={{ color: "#FF8E3C" }}>
-            Register here
-          </a>
-        </p>
-      </form>
+        <form className="login-form">
+          <input
+            type="text"
+            aria-label="username input"
+            placeholder="Enter Username"
+            onChange={handleUsername}
+            value={username}
+            className="inputField"
+          ></input>
+          <input
+            type="password"
+            aria-label="password input"
+            placeholder="Enter Password"
+            onChange={handlePassword}
+            value={password}
+            className="inputField"
+          ></input>
+          <br />
+          <button
+            onClick={gatherDetails}
+            id="submitBtn"
+            aria-label="submit button"
+            className="login-btn"
+          >
+            Submit
+          </button>
+          <p>
+            Not registered?{" "}
+            <a href="/register" style={{ color: "#FF8E3C" }}>
+              Register here
+            </a>
+          </p>
+        </form>
+      </div>
     </div>
   );
 };
