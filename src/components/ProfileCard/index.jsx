78c388e37a4c0997fff4e4b5049d1dd678afda05
Sean <sean_obeirne@outlook.com>
2023-06-16 12:32:17 +0100

    Simple set up from Profile Component that will be added to the ConnectionsPage

---
+++ src/components/ProfileCard/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ProfileCard = () => {
+    return (
+        <div>
+        <h1>Profile Card</h1>
+        </div>
+    );
+    }
+    export default ProfileCard;
