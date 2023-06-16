9a561082e6dc709e8e9d60c283c3698846c075ac
Muhammad Omar <omar@thecodingwhiz.com>
2023-06-16 16:14:40 +0100

    Mo and Sean frontEnd work

---
+++ src/components/ProfileCard/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProfileCard = () => {
+  return (
+    <div className="profile-card">
+      <h1>Profile Card</h1>
+      {/* Additional profile information */}
+    </div>
+  );
+};
+export default ProfileCard;
