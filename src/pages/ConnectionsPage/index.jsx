dd03614e64e7b4a5468eac4eab8c8f1097d879c1
Sean <sean_obeirne@outlook.com>
2023-06-16 12:26:26 +0100

    Simple boiler plate created for connections page

---
+++ src/pages/ConnectionsPage/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ConnectionsPage = () => {
+    return (
+        <div>
+        <h1>Connections Page</h1>
+        </div>
+    );
+    }
+
+export default ConnectionsPage;
