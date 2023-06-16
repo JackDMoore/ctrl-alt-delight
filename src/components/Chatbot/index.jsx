4ff3a1aef9bda7b5a615fc83ba4569370f82d0ca
Sean <sean_obeirne@outlook.com>
2023-06-16 12:20:58 +0100

    Chatbot updated

--- src/components/Chatbot/index.jsx
+++ src/components/Chatbot/index.jsx
@@ -1,66 +1,51 @@
-import React, { useState, useEffect } from "react";
-import Select from "react-select";
-import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
-import {
-  MainContainer,
-  ChatContainer,
-  MessageList,
-  Message,
-  MessageInput,
-  TypingIndicator,
-} from "@chatscope/chat-ui-kit-react";
-import ChatIcon from "../ChatIcon";
-// import "../../styles.css";
-// import "./Chatbot.css";
+import React, { useState, useEffect} from 'react'
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
+import ChatIcon from '../ChatIcon';
+import './Chatbot.css';
 
-const API_KEY = "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";
+const API_KEY = "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";
+// const API_KEY = process.env.REACT_APP_API_KEY;
 
-const systemMessage = {
-  role: "system",
-  content: "You are a chatbot translater. You job is to translate the message into the selected language and provide the english to so the user can see both languages to help them learn. ",
-};
+const systemMessage = { 
+  "role": "system", "content": "Translate my messages to French, provide the English too."
+}
+
 function Chatbot({ onClose }) {
-  const [selectedLanguage, setSelectedLanguage] = useState([]);
-  const [languageOptions, setLanguageOptions] = useState([]);
-
-  useEffect(() => {
-    //Could use the API?
-    const fetchedLanguageOptions = [
-      { value: "fr", label: "French" },
-      { value: "de", label: "German" },
-      { value: "es", label: "Spanish" },
-      { value: "it", label: "Italian" },
-    ];
-  
-  setLanguageOptions(fetchedLanguageOptions);
-}, []);
-
   const [messages, setMessages] = useState([
     {
-      message:
-        "Hello, I am LinguaBot. I am here to help you translate your messages while you game. Select your langauge to get started!",
+      message: "Hello, I am LinguaBot. I am here to help you translate your messages while you game.!",
       sentTime: "just now",
       sender: "ChatGPT",
-    },
+    }
   ]);
+
   const [isTyping, setIsTyping] = useState(false);
-  const [chatVisible, setChatVisible] = useState(false);
+//add use state so chat is visble on screen
+const [chatVisible, setChatVisible] = useState(false);
 
   const handleSend = async (message) => {
     const newMessage = {
       message,
-      direction: "outgoing",
-      sender: "user",
-      languauge: selectedLanguage.value,
+      direction: 'outgoing',
+      sender: "user"
     };
 
     const newMessages = [...messages, newMessage];
+    
     setMessages(newMessages);
+
+    // Initial system message to determine ChatGPT functionality
+    // How it responds, how it talks, etc.
     setIsTyping(true);
     await processMessageToChatGPT(newMessages);
   };
 
-  async function processMessageToChatGPT(chatMessages) {
+  async function processMessageToChatGPT(chatMessages) { // messages is an array of messages
+    // Format messages for chatGPT API
+    // API is expecting objects in format of { role: "user" or "assistant", "content": "message here"}
+    // So we need to reformat
+
     let apiMessages = chatMessages.map((messageObject) => {
       let role = "";
       if (messageObject.sender === "ChatGPT") {
@@ -68,42 +53,41 @@ function Chatbot({ onClose }) {
       } else {
         role = "user";
       }
-      return { role: role, content: messageObject.message };
+      return { role: role, content: messageObject.message}
     });
+
+
+    // Get the request body set up with the model we plan to use
+    // and the messages which we formatted above. We add a system message in the front to'
+    // determine how we want chatGPT to act. 
     const apiRequestBody = {
-      model: "gpt-3.5-turbo",
-      messages: [systemMessage, ...apiMessages],
-      language: selectedLanguage.value,
-    };
-  
-    try {
-      const response = await fetch("https://api.openai.com/v1/chat/completions", {
-        method: "POST",
-        headers: {
-          Authorization: "Bearer " + API_KEY,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(apiRequestBody),
-      });
-  
-      if (!response.ok) {
-        throw new Error("API Error: " + response.status);
-      }
-  
-      const data = await response.json();
-  
-      setMessages([
-        ...chatMessages,
-        {
-          message: data.choices[0].message.content,
-          sender: "ChatGPT",
-        },
-      ]);
-      setIsTyping(false);
-    } catch (error) {
-      console.error(error);
+      "model": "gpt-3.5-turbo",
+      "messages": [
+        systemMessage,  // The system message DEFINES the logic of our chatGPT
+        ...apiMessages // The messages from our chat with ChatGPT
+      ]
     }
+
+    await fetch("https://api.openai.com/v1/chat/completions", 
+    {
+      method: "POST",
+      headers: {
+        "Authorization": "Bearer " + API_KEY,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(apiRequestBody)
+    }).then((data) => {
+      return data.json();
+    }).then((data) => {
+      console.log(data);
+      setMessages([...chatMessages, {
+        message: data.choices[0].message.content,
+        sender: "ChatGPT"
+      }]);
+      setIsTyping(false);
+    });
   }
+
   const handleChatIconClick = () => {
     setChatVisible(true);
   };
@@ -112,56 +96,30 @@ function Chatbot({ onClose }) {
     onClose();
   };
 
+
   return (
     <div className="App">
       {chatVisible ? (
-        <div style={{ position: "fixed", bottom: "0", right: "20px" }}>
+        <div style={{ position: 'fixed', bottom: '0', right: '20px' }}>
           <MainContainer>
-            <ChatContainer className="chat-container"
-            style={{ height: "400px" }}>
-              <MessageList
-                scrollBehavior="smooth"
-                className="message-list"
-                typingIndicator={
-                  isTyping ? (
-                    <TypingIndicator
-                      className="typing-indicator"
-                      content="LinguaBot is typing"
-                    />
-                  ) : null
-                }
-              >
+            <ChatContainer className="chat-container">
+              <MessageList className="message-list" typingIndicator={isTyping ? <TypingIndicator className="typing-indicator" content="LinguaBot is typing" /> : null}>
                 {messages.map((message, i) => (
-                  <Message key={i} model={message}
-                  className={message.sender === "user" ? "user-message" : "bot-message"} />
+                  <Message key={i} model={message} />
                 ))}
               </MessageList>
-              {/* <div className="cs-message__content"></div> */}
-              <MessageInput
-                className="message-input"
-                placeholder="Type message here"
-                onSend={handleSend}
-              />
+              <MessageInput className="message-input" placeholder="Type message here" onSend={handleSend} />
             </ChatContainer>
           </MainContainer>
-          <button onClick={handleClose} style={{ position: "fixed", bottom: "405px", right: "20px"}}>Close</button>
+          <button onClick={handleClose}>Close</button>
         </div>
       ) : (
         <div className="chat-icon-container" onClick={handleChatIconClick}>
           <ChatIcon />
         </div>
       )}
-      {chatVisible && (
-        <div className= "languauge-dropdown-container" style={{ position: "fixed", bottom: "405px", right: "685px"}}>
-          <Select
-          options={languageOptions}
-            value = {selectedLanguage}
-            onChange = {setSelectedLanguage}
-            placeholder = "Select Language"
-          />
-          </div>
-      )}
     </div>
   );
 }
+
 export default Chatbot;
