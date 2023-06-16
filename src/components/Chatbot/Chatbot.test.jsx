004de7d5eb33b0cb93e7123d004d76ac8f9519ae
Sean <sean_obeirne@outlook.com>
2023-06-16 16:25:57 +0100

    Test files now running for Chat icon and Chat Bot, some fixes needed in chat bot, coverage 82.84

--- src/components/Chatbot/Chatbot.test.jsx
+++ src/components/Chatbot/Chatbot.test.jsx
@@ -1,20 +1,45 @@
 import React from "react";
-import { describe, it, expect, beforeEach, afterEach } from "vitest";
-import { screen, render, cleanup } from "@testing-library/react";
+import { describe, it, expect, beforeEach, afterEach, vi } from "vitest";
+import { screen, render, cleanup, fireEvent } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
 import matchers from "@testing-library/jest-dom/matchers";
-import userEvent from "@testing-library/user-event";
 expect.extend(matchers);
 
-import Chatbot from "./Chatbot";
+import Chatbot from ".";
 
 describe("Chatbot component", () => {
   beforeEach(() => {
-    render(<Chatbot />);
+    render(
+      <MemoryRouter>
+        <Chatbot />
+      </MemoryRouter>
+    );
   });
 
   afterEach(() => {
     cleanup();
   });
 
-  
+  it("renders the chat icon when chat is not visible", () => {
+    const chatIcon = screen.getByAltText("Chat Icon");
+    expect(chatIcon).toBeInTheDocument();
+  });
+
+  it("opens the chat on chat icon click", () => {
+    const chatIcon = screen.getByAltText("Chat Icon");
+    fireEvent.click(chatIcon);
+    const messageInput = screen.getByPlaceholderText("Type message here");
+    expect(messageInput).toBeInTheDocument();
+  });
+
+  it("closes the chat on close button click", () => {
+    const chatIcon = screen.getByAltText("Chat Icon");
+    fireEvent.click(chatIcon);
+    const closeButton = screen.getByText("Close");
+    fireEvent.click(closeButton);
+    const chatIconAfterClose = screen.getByAltText("Chat Icon");
+    expect(chatIconAfterClose).toBeInTheDocument();
+  });
+
+  // Add more tests to cover other functionality and edge cases of the Chatbot component
 });
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,3 +2,5 @@ export {default as PageWrapper} from "./PageWrapper"
 export { default as Hero } from "./Hero";
 export {default as Register} from "./Register"
 export {default as Login} from "./Login"
+export {default as ChatIcon} from "./ChatIcon"
+export {default as Chatbot} from "./Chatbot"
