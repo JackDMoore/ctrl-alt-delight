bf629e6aa2a71e7a756c45f690a8369bfb864258
Muhammad Omar <omar@thecodingwhiz.com>
2023-06-16 13:03:24 +0100

    Merge sean_dev branch into mo-dev2 branch

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,20 +1,10 @@
 import React from "react";
 import { Login } from "../../components";
-import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
-
 const LoginPage = () => {
-  const { userInfo } = useSelector((state) => state.auth);
-
-  if (userInfo) {
-    return <Navigate to="/profile" />;
-  }
-
   return (
     <div className="input-container">
       <Login />
     </div>
   );
 };
-
 export default LoginPage;
--- src/pages/RegisterPage/index.jsx
+++ src/pages/RegisterPage/index.jsx
@@ -1,20 +1,10 @@
 import React from "react";
 import { Register } from "../../components";
-import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
-
 const RegisterPage = () => {
-  const { userInfo } = useSelector((state) => state.auth);
-
-  if (userInfo) {
-    return <Navigate to="/profile" />;
-  }
-
   return (
     <div className="input-container">
       <Register />
     </div>
   );
 };
-
 export default RegisterPage;
