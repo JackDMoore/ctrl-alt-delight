c8f637a8a893b5f5142a2722342fe720b0efb89c
Jack Moore <jack.d.moore@outlook.comssh-keygen>
2023-06-19 16:35:21 +0100

    Register2 tests added

---
+++ src/components/Register2/Register2.test.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { describe, it, expect, beforeEach, afterEach, vi } from "vitest";
+import { screen, render, cleanup, fireEvent } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import matchers from "@testing-library/jest-dom/matchers";
+expect.extend(matchers);
+import axios from "axios";
+
+import Register2 from ".";
+
+describe("Register2 Component", () => {
+  beforeEach(() => {
+    render(
+      <MemoryRouter>
+        <Register2 />
+      </MemoryRouter>
+    );
+  });
+
+  afterEach(() => {
+    cleanup();
+  });
+
+  it("Displays a heading with text: Complete Registration", () => {
+    const heading = screen.getByText("Complete Registration");
+    expect(heading).toBeInTheDocument();
+  });
+
+  it("Displays text: Login here", () => {
+    const text = screen.getByText("Login here");
+    expect(text).toBeInTheDocument();
+  });
+
+  it("Username changes state when input value changes", () => {
+    const usernameInput = screen.getByLabelText("username input");
+    fireEvent.change(usernameInput, { target: { value: "Panda" } });
+    expect(usernameInput.value).toEqual("Panda");
+  });
+
+  it("Name changes state when input value changes", () => {
+    const nameInput = screen.getByLabelText("name input");
+    fireEvent.change(nameInput, { target: { value: "Panda" } });
+    expect(nameInput.value).toEqual("Panda");
+  });
+
+  it("Email changes state when input value changes", () => {
+    const emailInput = screen.getByLabelText("email input");
+    fireEvent.change(emailInput, { target: { value: "Panda" } });
+    expect(emailInput.value).toEqual("Panda");
+  });
+
+  it("Password changes state when the input value changes", () => {
+    const passwordInput = screen.getByLabelText("password input");
+    fireEvent.change(passwordInput, { target: { value: "areCool" } });
+    expect(passwordInput.value).toEqual("areCool");
+  });
+
+  it("is gatherDetails called after submit is clicked", () => {
+    vi.spyOn(axios, "post");
+    const submitButton = screen.getByLabelText("submit button");
+    submitButton.click();
+    expect(axios.post).toHaveBeenCalled();
+  });
+
+  it("is api called with button click?", () => {
+    const axiosspy = vi.spyOn(axios, "post");
+    const submitButton = screen.getByLabelText("submit button");
+    submitButton.click();
+    expect(axiosspy).toHaveBeenCalledWith(
+      "https://linguaplaya-be.onrender.com/signup",
+      expect.any(Object)
+    );
+  });
+});
