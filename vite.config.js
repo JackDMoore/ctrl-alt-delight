78e3162181cb9dd907ccab3f2a6a9609df0174ed
Kenen <62024117+kenenx@users.noreply.github.com>
2023-06-20 17:17:41 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import commonjs from '@rollup/plugin-commonjs';
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  plugins: [react(),commonjs()],
+  plugins: [react()],
   test: {
     environment: "jsdom",
   },
