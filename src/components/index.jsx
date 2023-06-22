6788a9ec414711a717751f5aa5cb415944769345
Muhammad Omar <omar@thecodingwhiz.com>
2023-06-22 16:16:25 +0100

    Merge to staging

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -7,15 +7,9 @@ export { default as ChatIcon } from "./ChatIcon";
 export { default as Chatbot } from "./Chatbot";
 export { default as Navbar } from "./Navbar";
 export { default as ProfileCard } from "./ProfileCard";
-<<<<<<< HEAD
 export { default as UserList } from "./userlist";
 export { default as ChatBody } from "./ChatBody";
 export { default as ChatFooter } from "./ChatFooter";
-=======
-export { default as UserList} from "./userlist";
-export { default as ChatBody} from "./ChatBody";
-export { default as ChatFooter} from "./ChatFooter";
->>>>>>> ab0d7797642a74b964b5c4e799e64bc4be27c74e
 export { default as ConnectionButton } from "./ConnectionButton";
 export { default as MessageButton } from "./MessageButton";
 export { default as LearnLanguageFilter } from "./LearnLanguageFilter";
